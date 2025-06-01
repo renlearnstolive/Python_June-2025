bef9e590dc9d5d3b31a02aa92606f126f30e0f0e
renlearnstolive <renkamiya45@gmail.com>
2025-06-01 20:46:32 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+mine = "to live a good life"
+your_dream = input("What's your dream?")
+print("mine is " + mine +". ")
+print("Yours is to " + your_dream + ". ")
+print("let\'s achieve it together")
+print("Thank you for being a witness to my growth.")
